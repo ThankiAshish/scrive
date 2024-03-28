24e16a9e0ce3f0a6d1f0ec24034d97849228536e
Ashish <thankiashish555@gmail.com>
2024-03-28 22:43:08 +0530

    Created Blog Context

---
+++ client/src/context/BlogContext.jsx
@@ -0,0 +1,39 @@
+import { createContext, useContext, useState, useEffect } from "react";
+import PropTypes from "prop-types";
+
+const BlogContext = createContext({});
+
+const BlogProvider = ({ children }) => {
+  const [blogs, setBlogs] = useState([]);
+
+  useEffect(() => {
+    const fetchBlogs = async () => {
+      const response = await fetch("/api/blog");
+
+      if (response.status === 200) {
+        const data = await response.json();
+        setBlogs(data.blogs);
+      } else {
+        setBlogs([]);
+      }
+    };
+
+    fetchBlogs();
+  }, []);
+
+  return (
+    <BlogContext.Provider value={{ blogs, setBlogs }}>
+      {children}
+    </BlogContext.Provider>
+  );
+};
+
+BlogProvider.propTypes = {
+  children: PropTypes.node.isRequired,
+};
+
+export const BlogState = () => {
+  return useContext(BlogContext);
+};
+
+export default BlogProvider;
