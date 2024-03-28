e3520b08123e798e05b8911bc74b71c9c9d081be
Ashish <thankiashish555@gmail.com>
2024-03-28 22:42:10 +0530

    Redefined the Structure of the Controllers and Services

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -1,48 +1,42 @@
 const userServices = require("../services/user.service");
 
-const registerController = async (req, res) => {
-  const { username, email, password, profilePicture } = req.body;
-  try {
-    await userServices.register(username, email, password, profilePicture);
-    res.status(201).json({ message: "Registration Successful!" });
-  } catch (error) {
-    res.status(400).json({ message: error.message });
-  }
+const userController = {
+  register: async (req, res) => {
+    const { username, email, password, profilePicture } = req.body;
+    try {
+      await userServices.register(username, email, password, profilePicture);
+      res.status(201).json({ message: "Registration Successful!" });
+    } catch (error) {
+      res.status(400).json({ message: error.message });
+    }
+  },
+  login: async (req, res) => {
+    const { email, password } = req.body;
+    try {
+      const user = await userServices.login(email, password);
+      res.status(200).json({ message: "Login Successful!", user: user });
+    } catch (error) {
+      res.status(400).json({ message: error.message });
+    }
+  },
+  getUser: async (req, res) => {
+    try {
+      const user = await userServices.getUser(req.user._id);
+      res.status(200).json({ user: user });
+    } catch (error) {
+      res.status(400).json({ message: error.message });
+    }
+  },
+  updateUser: async (req, res) => {
+    try {
+      const { username, email } = req.body;
+      const user = await userServices.updateUser(req.user._id, username, email);
+
+      res.status(200).json({ user: user, message: "User Details Updated!" });
+    } catch (error) {
+      res.status(400).json({ message: error.message });
+    }
+  },
 };
 
-const loginController = async (req, res) => {
-  const { email, password } = req.body;
-  try {
-    const user = await userServices.login(email, password);
-    res.status(200).json({ message: "Login Successful!", user: user });
-  } catch (error) {
-    res.status(400).json({ message: error.message });
-  }
-};
-
-const getUserController = async (req, res) => {
-  try {
-    const user = await userServices.getUser(req.user._id);
-    res.status(200).json({ user: user });
-  } catch (error) {
-    res.status(400).json({ message: error.message });
-  }
-};
-
-const updateUserController = async (req, res) => {
-  try {
-    const { username, email } = req.body;
-    const user = await userServices.updateUser(req.user._id, username, email);
-
-    res.status(200).json({ user: user, message: "User Details Updated!" });
-  } catch (error) {
-    res.status(400).json({ message: error.message });
-  }
-};
-
-module.exports = {
-  registerController,
-  loginController,
-  getUserController,
-  updateUserController,
-};
+module.exports = userController;
--- server/services/user.service.js
+++ server/services/user.service.js
@@ -2,95 +2,89 @@ const User = require("../models/user.model");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 
-const register = async (username, email, password, profilePicture) => {
-  const existingUser = await User.findOne({ email });
-  const existingUsername = await User.findOne({ username });
+const userServices = {
+  register: async (username, email, password, profilePicture) => {
+    const existingUser = await User.findOne({ email });
+    const existingUsername = await User.findOne({ username });
 
-  if (existingUser) {
-    throw new Error("User already exists");
-  }
+    if (existingUser) {
+      throw new Error("User already exists");
+    }
 
-  if (existingUsername) {
-    throw new Error("Username already exists");
-  }
+    if (existingUsername) {
+      throw new Error("Username already exists");
+    }
 
-  const salt = bcrypt.genSaltSync(10);
-  const hashedPassword = bcrypt.hashSync(password, salt);
+    const salt = bcrypt.genSaltSync(10);
+    const hashedPassword = bcrypt.hashSync(password, salt);
 
-  const user = new User({
-    username,
-    email,
-    password: hashedPassword,
-    profilePicture,
-  });
+    const user = new User({
+      username,
+      email,
+      password: hashedPassword,
+      profilePicture,
+    });
 
-  await user.save();
+    await user.save();
 
-  return user;
-};
+    return user;
+  },
+  login: async (email, password) => {
+    let user = await User.findOne({ email });
 
-const login = async (email, password) => {
-  let user = await User.findOne({ email });
+    if (!user) {
+      throw new Error("Invalid credentials");
+    }
 
-  if (!user) {
-    throw new Error("Invalid credentials");
-  }
+    const isPasswordValid = bcrypt.compareSync(password, user.password);
 
-  const isPasswordValid = bcrypt.compareSync(password, user.password);
+    if (!isPasswordValid) {
+      throw new Error("Invalid credentials");
+    }
 
-  if (!isPasswordValid) {
-    throw new Error("Invalid credentials");
-  }
+    const payload = {
+      user: {
+        _id: user._id,
+      },
+    };
 
-  const payload = {
-    user: {
+    const token = jwt.sign(payload, process.env.JWT_SECRET, {
+      expiresIn: "7d",
+    });
+
+    user = {
       _id: user._id,
-    },
-  };
+      username: user.username,
+      email: user.email,
+      profilePicture: user.profilePicture,
+      token,
+    };
 
-  const token = jwt.sign(payload, process.env.JWT_SECRET, {
-    expiresIn: "7d",
-  });
+    return user;
+  },
+  getUser: async (id) => {
+    const user = await User.findById(id).select("-password");
 
-  user = {
-    _id: user._id,
-    username: user.username,
-    email: user.email,
-    profilePicture: user.profilePicture,
-    token,
-  };
+    if (!user) {
+      throw new Error("User not found");
+    }
 
-  return user;
+    return user;
+  },
+  updateUser: async (id, username, email) => {
+    const user = await User.findById(id);
+
+    if (!user) {
+      throw new Error("User not found");
+    }
+
+    user.username = username;
+    user.email = email;
+
+    await user.save();
+
+    return user;
+  },
 };
 
-const getUser = async (id) => {
-  const user = await User.findById(id).select("-password");
-
-  if (!user) {
-    throw new Error("User not found");
-  }
-
-  return user;
-};
-
-const updateUser = async (id, username, email) => {
-  const user = await User.findById(id);
-
-  if (!user) {
-    throw new Error("User not found");
-  }
-
-  user.username = username;
-  user.email = email;
-
-  await user.save();
-
-  return user;
-};
-
-module.exports = {
-  register,
-  login,
-  getUser,
-  updateUser,
-};
+module.exports = userServices;
