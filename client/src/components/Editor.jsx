1e95f3e32c0497da005b762070af31d928ad29e5
Ashish <thankiashish555@gmail.com>
2024-03-29 11:41:10 +0530

    Added Image Option in Editor

--- client/src/components/Editor.jsx
+++ client/src/components/Editor.jsx
@@ -4,12 +4,28 @@ import PropTypes from "prop-types";
 import "react-quill/dist/quill.snow.css";
 
 const Editor = ({ value, onChange }) => {
+  const modules = {
+    toolbar: [
+      [{ header: [1, 2, false] }],
+      ["bold", "italic", "underline", "strike", "blockquote"],
+      [
+        { list: "ordered" },
+        { list: "bullet" },
+        { indent: "-1" },
+        { indent: "+1" },
+      ],
+      ["link", "image"],
+      ["clean"],
+    ],
+  };
+
   return (
     <ReactQuill
       value={value}
       onChange={onChange}
       theme="snow"
       className="quill"
+      modules={modules}
     />
   );
 };
